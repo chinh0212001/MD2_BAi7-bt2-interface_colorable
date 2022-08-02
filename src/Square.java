4869ce0d42d4c120b0139d0a60facd304158fc66
unknown <duongducchinh160@gmail.com>
2022-08-02 17:27:03 +0700

    interface_colorable

---
+++ src/Square.java
@@ -0,0 +1,50 @@
+public class Square extends Shape implements IColorable{
+    private double side;
+    public Square() {
+    }
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public Square(double side, String color, boolean filled) {
+        super(color,filled);
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+    public double getArea() {
+        return side * side;
+    }
+
+    @Override
+    public String toString() {
+        return "A Square with side="
+                + getSide()
+                + ", which is a subclass of "
+                + super.toString()
+                + ", Area=" +
+                + getArea();
+    }
+
+
+//
+//    @Override
+//    public void resize(double percent) {
+//        this.side *= (percent/100);
+//    }
+
+    @Override
+    public void howToColor() {
+        System.out.println("Square: Full Side...");
+
+
+
+    }
+}
